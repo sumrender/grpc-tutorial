8a5c0e494a1123996e3d081be84c53819cdd3692
sumrender <sumrender.singh@zenegy.dev>
2025-05-29 21:41:52 +0530

    works

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("http://localhost:5057");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "I am thala" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Quitting...");
